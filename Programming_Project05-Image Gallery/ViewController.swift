90d1dda5af9e0cee6a33e7ae00e2d55db29a4afc
Michel Deiman <michel.deiman@live.nl>
2018-01-13 12:24:55 +0100

    Initial Commit

---
+++ Programming_Project05-Image Gallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Programming_Project05-Image Gallery
+//
+//  Created by Michel Deiman on 13/01/2018.
+//  Copyright © 2018 Michel Deiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
